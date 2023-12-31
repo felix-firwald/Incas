9e59f6c88bc132291b7b0bbfb8d7b5a10b8983ee
Дмитрий Магон <93208917+felix-firwald@users.noreply.github.com>
2023-12-31 16:24:20 +0300

    redesign menu and created SelectionBox

--- Models/CustomTable.cs
+++ Models/CustomTable.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using Incubator_2.Common;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,15 +12,20 @@ namespace Incubator_2.Models
     struct CustomField
     {
         public string name;
-        public string viewName;
-        
+        public string view_name;
+        public string type;
+        public bool not_null;
+        public bool is_id;
+        public bool is_uniq;
+        public string fktable;
+        public string fkfield;
     }
     class CustomTable : Model
     {
         public int id { get; set; }
         public string name { get; set; }
         public string viewName { get; set; }
-        public bool fields { get; set; }
+        public List<CustomField> fields { get; set; }
 
         public CustomTable()
         {
@@ -36,10 +42,51 @@ namespace Incubator_2.Models
             {
                 CustomTable table = new CustomTable();
                 table.Serialize(dr);
+                table.fields = DeserializeFields(dr["fields"].ToString());
                 tables.Add(table);
             }
             return tables;
         }
-
+        private List<CustomField> DeserializeFields(string str)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<CustomField>>(str);
+        }
+        private string SerializeFields()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this.fields);
+        }
+        public CustomTable AddCustomTable()
+        {
+            StartCommand().Insert(new Dictionary<string, string>
+            {
+                { "name", $"'{name}'" },
+                { "viewName", $"'{viewName}'" },
+                { "fields", $"'{SerializeFields()}'" },
+            }).ExecuteVoid();
+            return this;
+        }
+        public CustomTable Update()
+        {
+            StartCommand()
+                .Update("viewName", name)
+                .Update("fields", SerializeFields())
+                .WhereEqual("id", id.ToString())
+                .ExecuteVoid();
+            return this;
+        }
+        public CustomTable GetCustomTableByName()
+        {
+            DataRow dr = StartCommand()
+                .Select()
+                .WhereEqual("name", name)
+                .ExecuteOne();
+            Serialize(dr);
+            return this;
+        }
+        public void RemoveCustomTable()
+        {
+            StartCommand().Delete().WhereEqual("id", id.ToString());
+        }
+        
     }
 }
