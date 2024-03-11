f5334cd151fd693ce895e24781ad508d1c4e4585
Дмитрий Магон <93208917+felix-firwald@users.noreply.github.com>
2024-03-11 13:31:08 +0300

    ++

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //   Номер сборки
 //      Редакция
 //
-[assembly: AssemblyVersion("0.9.6.2")]
-[assembly: AssemblyFileVersion("0.9.6.2")]
+[assembly: AssemblyVersion("0.9.7.1")]
+[assembly: AssemblyFileVersion("0.9.7.1")]
